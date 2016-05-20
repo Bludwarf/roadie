a1da7fc0331086c19e0826619d2e508b59140f8b
Bludwarf <bludwarf@gmail.com>
2016-05-20 18:17:27 +0200

    Fixes #1
    Stocker temporairement les mp3 récents sur le serveur

--- api/models/sql/migration/#1/1-addGooleFileId.sql
+++ api/models/sql/migration/#1/1-addGooleFileId.sql
@@ -1,1 +1,3 @@
+ALTER TABLE `enregistrement`
+	ADD COLUMN `googleFileId` VARCHAR(50) NULL DEFAULT NULL COMMENT 'Id du fichier dans Google' AFTER `morceau`;
 UPDATE enregistrement SET googleFileId = SUBSTRING_INDEX(fichier,'=',-1);
