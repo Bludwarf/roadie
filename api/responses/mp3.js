d06599f2b17c709f86109c3abccbdf9c4354475c
Bludwarf <bludwarf@gmail.com>
2017-03-20 18:53:22 +0100

    Lecture avec HTTP Partial-Content réel

--- api/responses/mp3.js
+++ api/responses/mp3.js
@@ -36,31 +36,57 @@ module.exports = function mp3(file, filename) {
     function stat(cb) {
       fs.stat(file, cb);
     },
+    
+    function range(stats, cb) {
+      // Prise en compte du Content-Range client
+      // exemple début VLC : 'bytes=0-'
+      // exemple milieu VLC : 'bytes=1536274-'
+      var range = {start: 0, end: stats.size-1, length: stats.size};
+      if (req.headers.range) {
+        var rx = /bytes=(\d*)-(\d*)/i;
+        var rs = rx.exec(req.headers.range);
+        range.start = parseInt(rs[1]) || 0;
+        range.end = parseInt(rs[2]) || stats.size-1;
+        range.length = range.end - range.start + 1;
+      }
+      cb(null, stats, range);
+    },
 
     /**
      *
      * @param stats {{mtime}}
      * @param cb
      */
-    function readFile(stats, cb) {
-      fs.readFile(file, function(e, data) {
+    function readFile(stats, range, cb) {
+      
+      cb(null, stats, range, fs.createReadStream(file, {
+        start: range.start,
+        end: range.end
+      }));
+      
+      /*fs.readFile(file, function(e, data) {
         if (e) return cb(e);
-        return cb(null, stats, data);
-      });
+        return cb(null, stats, range, data);
+      });*/
     }
 
-  ], function(err, stats, data) {
+  ], function(err, stats, range, stream) {
     if (err) return res.serverError(err);
-	var headers = {
+    
+    var headers = {
       'Accept-Ranges': 'bytes', // src : http://stackoverflow.com/questions/14304642/transcoding-and-streaming-audio-how-to-send-content-range-headers
-      'Content-Length': data.length,
-      'Content-Range': 'bytes 0-'+(data.length-1)+'/'+data.length, // TODO : prendre en compte le Range demandé par le client
+      'Connection': 'keep-alive',
+      'Content-Length': range.length,
+      'Content-Range': 'bytes '+range.start+'-'+range.end+'/'+range.length,
       'Content-Type': 'audio/mpeg',
       'Last-Modified': new Date(stats.mtime) // exemple "Mon, 10 Jan 2011 18:23:27 GMT"
     };
-	if (filename) headers['Content-Disposition'] = 'attachment; filename="'+filename+'.mp3"';
+    
+    if (filename) {
+      headers['Content-Disposition'] = 'attachment; filename="'+filename+'.mp3"';
+    }
     res.writeHead(206, headers);
-    res.end(data, 'binary');
+    stream.pipe(res); // http://stackoverflow.com/a/36729528/1655155
   });
 
 };
